b16eea32ef9934a7a76e07405447013850f7b3ae
karuntos <s32190083@student.ubm.ac.id>
2022-04-06 21:51:04 +0700

    modul buku.py untuk tabel buku

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-# Hello World!
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from anggota import *
+from buku import *
+from kembali import *
+from pinjaman import *
+
+engine = create_engine("sqlite:///dbPerpusOn.db")
+app = Flask(__name__)
+
+anggota_helper = AnggotaHelper(engine)
+buku_helper = BukuHelper(engine)
+kembali_helper = KembaliHelper(engine)
+pinjaman_helper = PinajmanHelper(engine)
+
+
